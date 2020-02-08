e642c324824c809c2afa84ac247e319bb97c2620
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-08 21:29:51 +0100

    Add lists

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -97,6 +97,18 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Things to do <sup>next week!</sup>',
+    description: 'Interesting things I want to check out next week!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+  {
+    id: 'list-3',
+    title: 'Things to do <sup>next month!</sup>',
+    description: 'Interesting things I want to check out next month!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
 ];
 
 const columns = [

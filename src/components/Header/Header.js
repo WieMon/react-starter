d2003fed55b5eec67d3355112131a07b564b4a83
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-02-18 10:40:13 +0100

    Fix and add colors to SearchResults

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import {NavLink, Link} from 'react-router-dom';
 import styles from './Header.scss';
 import Container from '../Container/Container.js';
 import Icon from '../Icon/Icon.js';
-import Search from '../Search/Search.js';
+import Search from '../Search/SearchContainer.js';
 
 class Header extends React.Component {
   render () {
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 import Container from '../Container/Container';
 import Card from '../Card/Card.js';
-
+import styles from './SearchResults.scss';
 
 class SearchResults extends React.Component {
   static propTypes = {
@@ -19,9 +19,9 @@ class SearchResults extends React.Component {
 
     return (
       <Container>
-        <section>
+        <section className={styles.wrapper}>
           <div>
-            <h2>Card</h2>
+            <h2>Cards</h2>
             {cards.map(cardData => (
               <Card key={cardData.id} {...cardData} />
             ))}
